dd945f984dcb17fa26c5c6b1a81c274634f889d2
salesmiguelz <miguelsales02@gmail.com>
2025-04-26 15:44:38 -0300

    feat: added task component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./global.css"
 import { Input } from "./components/Input"
+import { Task } from "./components/Task"
 
 function App() {
   return (
     <>
       <Input
         placeholder="Adicione uma nova tarefa"
+      />
+      <Task 
       />
     </>
   )
---
+++ src/components/Task.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { MdDone } from "react-icons/md";
+
+import styles from "./Task.module.css"
+
+export function Task(){
+    const [taskDone, setTaskDone] = useState(true);
+
+    function handleTaskStatus(){
+        setTaskDone(!taskDone)
+    }
+
+    return (
+        <>
+            <div className={styles.task} onClick={handleTaskStatus}>
+                <div className={styles.taskStatusIcon}>
+                    {
+                        taskDone ? (
+                            <div className={styles.taskDoneIcon}>
+                                <MdDone 
+                                    size={16}
+                                />
+                            </div>
+                        ) : (
+                            <div className={styles.taskPendingIcon}></div>
+                        )
+                    }
+                </div>
+                <div className={taskDone ? styles.taskCrossedText : styles.taskText}>
+                    <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+                </div>
+
+                <div className={styles.taskTrashIcon}>
+                    <FaRegTrashAlt 
+                        size={17}
+                    />
+                </div>
+            </div>
+        </>
+
+    )
+}
