dbb07901f2d5aa280f98af3ec553a8f09b099483
salesmiguelz <miguelsales02@gmail.com>
2025-04-26 13:08:32 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./global.css"
+import { Input } from "./components/Input"
+
+function App() {
+  return (
+    <>
+      <Input
+        placeholder="Adicione uma nova tarefa"
+      />
+    </>
+  )
+}
+
+export default App
