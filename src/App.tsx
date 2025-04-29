a5d3041dd125cdd5d248b9013dff10b411f536d6
salesmiguelz <miguelsales02@gmail.com>
2025-04-29 16:53:33 -0300

    feat: added tasks management on local storage

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,30 @@ function App() {
   const [tasks, setTasks] = useState<TaskType[]>([]);
 
   useEffect(() => {
+    const storedTasks = Object.keys(localStorage)
+      .filter(key => key.startsWith('task-'))
+      .map(key => JSON.parse(localStorage.getItem(key) || ''))
+    
+    setTasks(storedTasks);
+  }, []);
+
+  useEffect(() => {
     if (confirmTaskCreation && taskTitle) {
+      const task = {
+        id: crypto.randomUUID(),
+        title: taskTitle,
+        isDone: false,
+        deleteTask: handleDeleteTask,
+        toggleTaskDone: handleToggleTaskDone
+      }
       setTasks(prev => [
         ...prev,
-        {
-          id: crypto.randomUUID(),
-          title: taskTitle,
-          isDone: false,
-          deleteTask: handleDeleteTask,
-          toggleTaskDone: handleToggleTaskDone
-        }
+        task
       ]);
 
       setConfirmTaskCreation(false);
       setTaskTitle('');
+      localStorage.setItem(`task-${task.id}`, JSON.stringify(task))
     }
   }, [confirmTaskCreation, taskTitle]);
 
@@ -36,16 +46,25 @@ function App() {
  }
 
  function handleToggleTaskDone(taskId: string) {
-  setTasks(prevTasks =>
-    prevTasks.map(task =>
-      task.id === taskId ? { ...task, isDone: !task.isDone } : task
-    )
-  );
+  setTasks(prevTasks => {
+    const updatedTasks = prevTasks.map(task => {
+      if (task.id === taskId) {
+        const updatedTask = { ...task, isDone: !task.isDone };
+        localStorage.setItem(`task-${taskId}`, JSON.stringify(updatedTask));
+        return updatedTask;
+      }
+      return task;
+    });
+
+    return updatedTasks;
+  });
 }
 
   function handleDeleteTask(taskId: string){
     setTasks(tasks => {
-      return tasks.filter(task => task.id !== taskId)
+      const updatedTasks = tasks.filter(task => task.id !== taskId);
+      localStorage.removeItem(`task-${taskId}`);  
+      return updatedTasks;
     })
   }
   return (
